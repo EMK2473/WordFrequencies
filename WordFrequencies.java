964cfab37d367406f26fddbd99388837cc77314f
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-10 15:37:58 -0400

    dev'd main method to scan input line; create an array of strings by splitting input line by whitespace; declared and assigned arraysize by string array length; for each string in stringArray ; loop abd print string for testing

--- WordFrequencies.java
+++ WordFrequencies.java
@@ -1,3 +1,5 @@
+package WordFrequencies;
+
 import java.util.Scanner;
 
 public class WordFrequencies {
@@ -6,5 +8,18 @@ public class WordFrequencies {
 
    public static void main(String[] args) {
       /* Type your code here. */
+
+      Scanner scnr = new Scanner(System.in);
+        String inputLine = scnr.nextLine();
+        String[] stringArray = inputLine.split(" ");
+        int arraySize = stringArray.length;
+
+        System.out.print("Array of strings: ");
+        // for each syntax
+        for(String s: stringArray) {
+            System.out.print(s + " \n");
+        }
+        System.out. println("Array size: " + arraySize);
+      scnr.close();
    }
 }
