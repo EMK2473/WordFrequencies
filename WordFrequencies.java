7eaca54079c12d09a102cfb48b908aa3f66a26ee
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-10 15:13:16 -0400

    init file

---
+++ WordFrequencies.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class WordFrequencies {
+
+   /* Define your method here */
+
+   public static void main(String[] args) {
+      /* Type your code here. */
+   }
+}
